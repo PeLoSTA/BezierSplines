46cc0e329e672c335a064fe755bd6ae147d58d43
Peter Loos <peter.loos@gmx.de>
2016-11-01 16:21:33 +0100

    Changed background color to red in feat 3

--- app/src/main/java/de/peterloos/beziersplines/views/BezierView.java
+++ app/src/main/java/de/peterloos/beziersplines/views/BezierView.java
@@ -88,8 +88,7 @@ public class BezierView extends View implements View.OnTouchListener {
         this.resolution = 50;
         this.constructionPosition = (float) 0.5;
 
-        this.setBackgroundColor(Color.YELLOW);
-        this.setBackgroundColor(Color.GREEN);
+        this.setBackgroundColor(Color.RED);   // feat 03 -- should be Red
     }
 
     // getter/setter
