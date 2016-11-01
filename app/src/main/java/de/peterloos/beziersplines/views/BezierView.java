d22f54468f1319921d1773d485fc08914290bb5c
Peter Loos <peter.loos@gmx.de>
2016-11-01 16:20:19 +0100

    Changed background color to yellow in feat 1

--- app/src/main/java/de/peterloos/beziersplines/views/BezierView.java
+++ app/src/main/java/de/peterloos/beziersplines/views/BezierView.java
@@ -88,8 +88,7 @@ public class BezierView extends View implements View.OnTouchListener {
         this.resolution = 50;
         this.constructionPosition = (float) 0.5;
 
-        this.setBackgroundColor(Color.YELLOW);
-        this.setBackgroundColor(Color.GREEN);
+        this.setBackgroundColor(Color.YELLOW);   // feat 01 -- should be Yellow
     }
 
     // getter/setter
