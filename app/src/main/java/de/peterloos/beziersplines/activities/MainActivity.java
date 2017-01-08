7e572846749237c9f3735313d85c37db250e9a3b
Peter Loos <peter.loos@gmx.de>
2017-01-08 15:43:21 +0100

    Weiterarbeit an MainActivity

--- app/src/main/java/de/peterloos/beziersplines/activities/MainActivity.java
+++ app/src/main/java/de/peterloos/beziersplines/activities/MainActivity.java
@@ -26,8 +26,6 @@ import de.peterloos.beziersplines.R;
 
 // TODO: Die Seekbars sind recht ungenau ... für schrittweise könnte man auch eine Stepwise Control benötigen#
 
-// TODO: Starte jetzt mit Material Layout .. und jetzt erst Recht
-// TODO: Und das ist jetzt noch eine Zeile darüber
 // TODO: Starte jetzt mit Material Layout
 // TODO: Die AsyncTask sollte als dritten Paramer Void bekommen ...
 // TODO: Die AsyncTask sollte als ersten Paramer Void bekommen ...
