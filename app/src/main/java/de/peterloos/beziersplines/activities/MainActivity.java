731d36e075ec46c51bf2e2d3cf9a8f7b208581eb
PeLoSTA <info@peterloos.de>
2017-01-08 10:54:16 +0100

    Did a change on the server

--- app/src/main/java/de/peterloos/beziersplines/activities/MainActivity.java
+++ app/src/main/java/de/peterloos/beziersplines/activities/MainActivity.java
@@ -26,7 +26,7 @@ import de.peterloos.beziersplines.R;
 
 // TODO: Die Seekbars sind recht ungenau ... für schrittweise könnte man auch eine Stepwise Control benötigen#
 
-// TODO: Starte jetzt mit Material Layout
+// TODO: Starte jetzt mit Material Layout .. und jetzt erst Recht
 // TODO: Die AsyncTask sollte als dritten Paramer Void bekommen ...
 // TODO: Die AsyncTask sollte als ersten Paramer Void bekommen ...
 
